c8aff34f74082da3b6f8a94ad47e967d6487972e
Vinit Kanani <kananivinit97@gmail.com>
2017-10-22 15:53:13 +0530

    Initial Commit

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 from user import views
 
 urlpatterns = [
+
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': {'user:login'}}, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
